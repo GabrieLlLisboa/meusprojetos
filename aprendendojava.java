0a2f2468c63ec49f8ac8b632ff4f392b65fee0c8
Gabriel Lisboa <devopup08@gmail.com>
2025-08-25 10:24:27 -0300

    Update aprendendojava.java

--- aprendendojava.java
+++ aprendendojava.java
@@ -1,20 +1,26 @@
 public class aprendendojava{
     public static void main(String[] args) {
 
+        try {
 
         int idade = 30;
-        str nome = ("Samuel");
+        String nome = "Samuel";
 
         System.out.println("Mostrando suas informações...");
         Thread.sleep(500);
-        System.out.println("Seu nome é:" + nome);
-        System.out.println("Sua idade é:" + idade);
-        Thread.sleep(500);
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua idade é: " + idade);
+        Thread.sleep(1000);
         System.out.println("Fim");
 
 
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
 
         }
 
-    }
+        }
+
 
